f5f96606420e1a0580d0ac79d874fd42dc709f9b
jiri bily <jiri.bily@academicschool.cz>
2026-01-21 10:46:25 +0100

    OOP basic c#

---
+++ 1_Pololetí/OOPBasic/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+
+
+/*
+Student student = new Student(16, "Jirka");
+student.Greet();
+ChangeStudent(student);
+student.Greet();
+
+void ChangeStudent(Student s)
+{
+    s.name = "Nothing";
+}
+*/
+
+wall wall = new wall(1, 2, "cement");
+wall.PrintInfo();
+
+wall wall1 =  new wall(3, 2, "wood");
+
+wall1.PrintInfo();
---
+++ 1_Pololetí/OOPBasic/wall.cs
@@ -0,0 +1,26 @@
+﻿class wall
+{
+    public float height;
+    public float width;
+    public string material;
+
+    public wall(float height, float width, string material)
+    {
+        this.height = height;
+        this.width = width;
+        this.material = material;
+    }
+
+    public float GetSurface()
+    {
+        return height * width;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine(" Wall material: " + material + ", Wall surface: " +  GetSurface() + "^2");
+
+
+    }
+}
+
