e190226cc4dfaec8ea8df6aa2f372c624ed45324
jiri bily <jiri.bily@academicschool.cz>
2026-06-03 08:28:24 +0200

    zalozeni a trida card

---
+++ Final project/karta.cs
@@ -0,0 +1,17 @@
+﻿abstract class card
+{
+    string cislo { get; set; } 
+    int hodnota { get; set; }
+    string znak {get; set;}
+
+    public virtual void randomcard()
+    {
+        List<string> cisla = new List<string> {"A","2","3","4","5","6","7","8","9","10","J","Q","K" };
+        Random nahoda = new Random();
+        // generuje nahodne cislo od nuly do poctu cisel v listu 
+        int nahodnyIndex = nahoda.Next(cisla.Count);
+        
+        string vybraneCislo = cisla[nahodnyIndex]; 
+        cislo = vybraneCislo;
+    }
+}
